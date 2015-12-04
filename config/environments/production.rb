7fc1632a5267c52dbd4e15d278345d4ca250d911
blwasham <bryanwasham@hotmail.com>
2015-12-03 21:39:59 -0500

    tweak to prod env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,6 @@ Jackieart::Application.configure do
 
   # See everything in the log (default is :info)
   config.log_level = :info
-  
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
 
