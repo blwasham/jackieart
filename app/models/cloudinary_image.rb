e76d6f1842e458e660a1213db45cdfbe63877e80
blwasham <bryanwasham@hotmail.com>
2015-12-11 22:00:06 -0500

    updating active resource require syntax

--- app/models/cloudinary_image.rb
+++ app/models/cloudinary_image.rb
@@ -1,4 +1,4 @@
-require ActiveResource
+require 'active_resource' 
 class CloudinaryImage < ActiveResource::Base
   self.site = "http://res.cloudinary.com/bwasham/image/upload/v1438215917/emma_phjyok.jpg"
 
